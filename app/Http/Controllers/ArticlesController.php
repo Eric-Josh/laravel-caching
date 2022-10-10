069ed804b34eb5fb2712b03d0cf12ed644b51e36
Eric-Josh <techjosmar@gmail.com>
2022-10-10 14:44:56 +0100

    init commit

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
+use App\Models\Article;
+
+class ArticlesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $page = 1;
+
+        if($request->has('page')) {
+            $page = $request->query('page');
+        }
+
+        $key = 'articles_'.$page;
+
+        return Cache::remember($key, 5, function () {
+            return Article::orderBy('id','desc')->paginate(5);
+        });
+    }
+
+    // Returns all 500 without Caching 
+    public function allWithoutCache()
+    {
+        return Article::all();
+    }
+
+    // with redis
+    public function withRedis(Request $request)
+    {
+        $page = 1;
+
+        if($request->has('page')) {
+            $page = $request->query('page');
+        }
+
+        $key = 'articles_'.$page;
+
+        return Cache::remember($key, 5, function () {
+            return Article::paginate(5);
+        });
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
